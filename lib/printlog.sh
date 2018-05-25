ef49349495d0214573e2071fe83840b5977e7a3c
Jeroen S <jeroen@declavinoot.nl>
2018-05-25 18:36:13 +0200

    New: printlog

---
+++ lib/printlog.sh
@@ -0,0 +1,48 @@
+# Shared print/log functions 
+declare VERBOSE=0
+declare QUIET=0
+
+declare PRINTLOG_DATEFORMAT='%Y-%m-%d %H:%M:%S'
+
+if [ "$APPNAME" = "" ]; then
+  APPNAME="shellscript"
+fi
+
+function print_log () {
+  local TIME=$(date +"$PRINTLOG_DATEFORMAT")
+  echo "[$TIME] $*"
+}
+
+function print_verbose () {
+  if [ "$VERBOSE" -gt 0 ]; then
+    if [ "$1" == "log" ]; then
+      shift 1
+      print_log "$*"
+    else
+      echo "$*"
+    fi
+  fi
+}
+
+function print_noquiet () {
+  if [ "$QUIET" -eq 0 ]; then
+    if [ "$1" == "log" ]; then
+      shift 1
+      print_log "$*"
+    else
+      echo "$*"
+    fi
+  fi
+}
+
+function print_app_log() {
+  local TIME=$(date +"$PRINTLOG_DATEFORMAT")
+  echo "[$TIME] $APPNAME: $*"
+}
+
+function print_app_verbose() {
+  if [ "$VERBOSE" -gt 0 ]; then
+    local TIME=$(date +"$PRINTLOG_DATEFORMAT")
+    echo "[$TIME] $APPNAME: $*"
+  fi
+}
