12d58f1a10e69514bed4cb396c3c2fb816c1099c
Jeroen S <jeroen@declavinoot.nl>
2018-05-26 04:14:43 +0200

    Fix: syslog-example

--- examples/syslog-example.sh
+++ examples/syslog-example.sh
@@ -7,17 +7,29 @@ require_lib syslog
 APPNAME=my-wonderfull-script
 
 
-# Open for pid.local1
+# First round.
+
 openlog $APPNAME pid local1
 
 syslog info "Running ${BASH_SOURCE[0]}"
 
+# See: printf style!
 BGNAME=Holiday
 NUMIMG=6
-
 syslog info "Background '%s' shall have %d images." $BGNAME $NUMIMG
 
 closelog
 
 echo "-- Look in /var/log/syslog to see the written logs. --"
+
+# Next round.
  
+openlog  $APPNAME pid local3
+
+syslog warning "%d connections from %s" 54 "spam.mer.in"
+
+syslog authpriv.err "intruder alert!"
+
+closelog
+
+echo "-- Look in /var/log/auth.log for the authpriv.err message. --"
